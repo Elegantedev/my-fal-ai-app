841fcc61cecbdfea2475db28567fc1200007ef32
Elegantedev <121271378+Elegantedev@users.noreply.github.com>
2024-08-22 01:37:45 +0200

    Create test.ts

---
+++ pages/api/test.ts
@@ -0,0 +1,7 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const testHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({ message: 'API is working' });
+};
+
+export default testHandler;
