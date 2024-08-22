b12841219cd62a1e1b6b9c88020cde8041df3bbf
Elegantedev <121271378+Elegantedev@users.noreply.github.com>
2024-08-22 02:03:22 +0200

    Delete pages directory

--- pages/api/test.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-const testHandler = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ message: 'API is working' });
-};
-
-export default testHandler;
